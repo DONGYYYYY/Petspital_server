4caf3ca9d7202d28eee6350032d55676439ca754
DongYoung <billy009100@naver.com>
2018-11-19 12:19:35 +0900

    change db name

--- src/main/java/com/sopoong/camp/vo/Pet.java
+++ src/main/java/com/sopoong/camp/vo/Pet.java
@@ -4,13 +4,13 @@ import lombok.Data;
 
 @Data
 public class Pet {
-	private String UserId;
-	private String PetId;
-	private String Name;
-	private String Age;
-	private String Weight;
-	private String Birth;
-	private String Infrom;
-	private String KING;
-	private int FLAG;
+	private String userid;
+	private String petid;
+	private String name;
+	private String age;
+	private String weight;
+	private String birth;
+	private String infrom;
+	private String kind;
+	private int flag;
 }
