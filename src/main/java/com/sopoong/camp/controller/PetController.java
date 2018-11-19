f19c9a30b85971fdb39fc1a23394b8c7597cc1f1
DongYoung <billy009100@naver.com>
2018-11-19 12:36:36 +0900

    aaa

--- src/main/java/com/sopoong/camp/controller/PetController.java
+++ src/main/java/com/sopoong/camp/controller/PetController.java
@@ -1,5 +1,7 @@
 package com.sopoong.camp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,33 +9,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sopoong.camp.dao.MemberDao;
-import com.sopoong.camp.vo.Comment;
+import com.sopoong.camp.dao.PetDao;
 import com.sopoong.camp.vo.Member;
+import com.sopoong.camp.vo.Pet;
 
 
 @RestController
 @RequestMapping("/Pet")
 public class PetController {
-	
-	@Autowired
-	MemberDao memberDao;
-	
-	@RequestMapping(value = "/AddPet",method=RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public int signUpMember(@RequestBody Member member){
-		
-		return memberDao.signUpMember(member);
-	}
+   
+   @Autowired
+   PetDao petDao;
+   
+   @RequestMapping(value = "/AddPet",method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+   public int AddPet(@RequestBody Pet pet){
+      
+      return petDao.AddPet(pet);
+   }
 
-	
-	@RequestMapping(value = "/GetPetList",method=RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public Member signInMember(@RequestBody Member member){
-		return memberDao.signInMember(member);
-	}
+   
+   @RequestMapping(value = "/GetPetList",method=RequestMethod.POST, produces="application/json;charset=UTF-8")
+   public List<Pet> signInMember(@RequestParam String userid){
+      return petDao.GetPetList(userid);
+   }
 
-	
-	@RequestMapping(value = "/DelPet",method = RequestMethod.POST,produces="application/json;charset=UTF-8")
-	public int deleteMember(@RequestBody Member member) {
-		return memberDao.deleteMember(member);
-	}
+   
+   @RequestMapping(value = "/UpdatePet",method = RequestMethod.POST,produces="application/json;charset=UTF-8")
+   public int deleteMember(@RequestBody Pet pet) {
+      return petDao.UpdatePet(pet);
+   }
 }
